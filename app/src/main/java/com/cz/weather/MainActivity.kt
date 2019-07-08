f25db8cb812c0bc04ed6172d3c185b2251bedeb7
ChueiZuoChen <louisa0103a@gmail.com>
2019-07-08 21:23:18 +0800

    Navigation with bottom navigate

--- app/src/main/java/com/cz/weather/MainActivity.kt
+++ app/src/main/java/com/cz/weather/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        navController = Navigation.findNavController(this,R.id.myNavHost)
+        navController =findNavController(R.id.myNavHost)
 
         //connect between navigation fragrament id and menu item id
         bottom_nav.setupWithNavController(navController)
